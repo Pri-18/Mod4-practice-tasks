682867b7d4a7740a554e15aba87e9d25fc798601
Pri-18 <priyansu.bhattacharya@innoraft.com>
2025-07-16 12:05:02 +0530

    Implement the changes

--- web/modules/custom/alter_route/src/Controller/Alter_routeController.php
+++ web/modules/custom/alter_route/src/Controller/Alter_routeController.php
@@ -1,11 +1,19 @@
 <?php
 
 namespace Drupal\alter_route\Controller;
-
 use Drupal\Core\Controller\ControllerBase;
 
+/**
+ * To alter the routes.
+ */
 class Alter_routeController extends ControllerBase {
 
+  /**
+   * Receives a dynamic node ID from the route parameter
+   * @param mixed $nodeId
+   * 
+   * @return array
+   */
   public function node($nodeId) {
     return [
       '#title' => $this->t('Node ID Page'),
