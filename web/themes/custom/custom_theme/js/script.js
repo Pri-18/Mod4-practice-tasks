9317ec42b8989579f695b15fca3a83e13c5eaf6d
Pri-18 <priyansu.bhattacharya@innoraft.com>
2025-08-07 17:11:14 +0530

    Resolve conflict

--- web/themes/custom/custom_theme/js/script.js
+++
@@ -1,7 +0,0 @@
-(function ($, Drupal) {
-  Drupal.behaviors.customThemeBehavior = {
-    attach: function (context, settings) {
-      console.log('My Bootstrap subtheme JS loaded!');
-    }
-  };
-})(jQuery, Drupal);
